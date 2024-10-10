e0ca57c0ec5ef550f1680c452a6508bd0e587ee7
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-10-10 15:13:37 +0530

    proj setup

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB', err.message);
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port, (err) => {
     if (err) {
